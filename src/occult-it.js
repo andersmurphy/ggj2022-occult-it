d8d8c0b01e4e0d5b304a53ea089026864135645e
Jock Findlay <jockfindlay@gmail.com>
2022-01-29 14:15:43 +0000

    make container

--- src/occult-it.js
+++ src/occult-it.js
@@ -1,15 +1,24 @@
 export class OccultIt {
     engine
+    gameContainer
+    pipe
 
     constructor(engine) {
         this.engine = engine
     }
 
     create() {
+        this.gameContainer = this.engine.makeContainer()
+        this.positionContainers()
 
     }
 
     update() {
         
+    }
+
+    positionContainers() {
+        this.gameContainer.x = 0;
+        this.gameContainer.y = 0;
     }
 }
