09d7210cd08669ea87393c1c5c0b91b095e1c57e
andersmurphy <andersmurphy@gmail.com>
2022-01-29 19:37:37 +0000

    Automatically connect to host (or become host)

--- src/network.js
+++ src/network.js
@@ -1,13 +1,28 @@
 import Peer from 'peerjs'
 
-let yourId = document.getElementById('yourId')
-
+let hostId = 'occult-2'
 let sendButton = document.getElementById('sendButton')
 let sendInput  = document.getElementById('sendInput')
-let connectButton = document.getElementById('connectButton')
-let userId = document.getElementById('userId')
 
-const setupConnection = (conn) => {
+let peer = new Peer(hostId)
+peer.on('error', function(err) {
+  peer = new Peer()
+  setTimeout(() =>
+    {
+      let conn = peer.connect(hostId)
+      conn.on('open', function() {
+        console.log('conneted to host!')
+        conn.on('data', function(data) {
+          console.log('Received', data)
+        })
+        // this is what we would use to send data to the other peer
+        sendButton.addEventListener(
+          'click', () => conn.send(sendInput.value), false)
+      })
+    }, 1000)
+})
+
+peer.on('connection', function(conn) {
   conn.on('open', function() {
     conn.on('data', function(data) {
       console.log('Received', data)
@@ -16,33 +31,4 @@ const setupConnection = (conn) => {
     sendButton.addEventListener(
       'click', () => conn.send(sendInput.value), false)
   })
-}
-
-const connectToOtherPeer = (peer, peerId) => {
-  conn = peer.connect(peerId)
-  setupConnection(conn)
-}
-
-connectButton.addEventListener(
-  'click', () => connectToOtherPeer(peer, userId.value), false)
-
-const setupPeer = (peer) => {
-  peer.on('connection', function(conn) {
-    setupConnection(conn)
-  })
-}
-
-let hostId = 'occult'
-let peer = new Peer(hostId)
-peer.on('open', function(id) {
-  yourId.textContent = id
 })
-
-peer.on('error', function(err) {
-  peer = new Peer()
-  peer.on('open', function(id) {
-    yourId.textContent = id
-  })
-})
-
-setupPeer(peer)
